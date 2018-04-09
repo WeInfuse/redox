6a64a40a5cf02c274057b4020102013a28b9bfa7
Pedro De Ona <pdeon003@fiu.edu>
2018-04-09 17:46:01 -0400

    linting

--- lib/redox-engine/patient.rb
+++ lib/redox-engine/patient.rb
@@ -2,8 +2,8 @@ module RedoxEngine
   # Serialize the Patient response object from RedoxEngine
   class Patient
     include Util
-    def initialize(patient_hash)
-      map_hash_to_attributes(patient_hash.rubyize_keys)
+    def initialize(patient_data)
+      map_hash_to_attributes(patient_data.rubyize_keys)
     end
   end
 end
--- test/redox-engine_tests/patient_test.rb
+++ test/redox-engine_tests/patient_test.rb
@@ -2,20 +2,22 @@ require 'test_helper'
 
 class PatientTest < Minitest::Test
   def test_attributes_map_from_response
-    pt = RedoxEngine::Patient.new(real_patient)
-    assert_equal [
-      {
-        id: '0000000001',
-        id_type: 'MR'
-      },
-      {
-        id: 'e167267c-16c9-4fe3-96ae-9cff5703e90a',
-        id_type: 'EHRID'
-      },
-      {
-        id: 'a1d4ee8aba494ca',
-        id_type: 'NIST'
-      }
-    ], pt.identifiers
+    VCR.use_cassette('client/new/token') do
+      pt = RedoxEngine::Patient.new(client: redox, patient_data: real_patient)
+      assert_equal [
+        {
+          id: '0000000001',
+          id_type: 'MR'
+        },
+        {
+          id: 'e167267c-16c9-4fe3-96ae-9cff5703e90a',
+          id_type: 'EHRID'
+        },
+        {
+          id: 'a1d4ee8aba494ca',
+          id_type: 'NIST'
+        }
+      ], pt.identifiers
+    end
   end
 end
