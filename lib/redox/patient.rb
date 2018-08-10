284ac27c188977472a73ee632df7056a3b2d3fbd
Alexander Clark <alexander.clark@weinfuse.com>
2018-08-10 12:25:02 -0500

    Revert "Merge branch 'master' of https://github.com/pdeona/redox into pdeona-master"
    This reverts commit 349d27f38963bde9a03655550f24b3d7d368aeea, reversing
    changes made to d06ee2fd0473012a814a90ad9c61a3732eba871b.

--- lib/redox/patient.rb
+++
@@ -1,9 +0,0 @@
-module Redox
-  # Serialize the Patient response object from Redox
-  class Patient
-    include Util
-    def initialize(patient_data)
-      map_hash_to_attributes(patient_data.rubyize_keys)
-    end
-  end
-end
