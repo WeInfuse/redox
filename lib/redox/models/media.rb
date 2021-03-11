da1e0bebce3f90a01cd1331901da9b90f6eaff8a
Mike Crockett <github@mmcrockett.com>
2021-03-11 09:58:08 -0600

    Tweaks to media model.

--- lib/redox/models/media.rb
+++ lib/redox/models/media.rb
@@ -1,12 +1,15 @@
 module Redox
   module Models
     class Media < AbstractModel
+      BLOB_REQUIRED_SIZE = 200 * 1024
+
       property :FileType, from: :file_type, required: false
       property :FileName, from: :file_name, required: false
       property :FileContents, from: :file_contents, required: false
       property :DocumentType, from: :document_type, required: false
       property :DocumentID, from: :document_id, required: false
       property :Availability, from: :availability, required: false
+      property :Provider, from: :provider, required: false
 
       alias_method  :file_type, :FileType
       alias_method  :file_name, :FileName
@@ -14,6 +17,28 @@ module Redox
       alias_method  :document_type, :DocumentType
       alias_method  :document_id, :DocumentID
       alias_method  :availability, :Availability
+      alias_method  :provider, :Provider
+
+      def availability=(value)
+        case value
+        when true
+          self[:Availability] = 'Available'
+        when false
+          self[:Availability] = 'Unavailable'
+        else
+          self[:Availability] = value
+        end
+      end
+
+      def add_filepath(path)
+        if File.size(path) > BLOB_REQUIRED_SIZE
+          raise 'Not implemented'
+        else
+          self.file_contents = Base64::encode64(File.read(path))
+          self.file_type     = "#{File.extname(path)}".delete_prefix('.').upcase
+          self.file_name     = File.basename(path, '.*')
+        end
+      end
     end
   end
 end
---
+++ test/redox/models/media_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+
+class MediaTest < Minitest::Test
+  describe 'media' do
+    let(:media) { Redox::Models::Media.new }
+
+    describe '#availablity=' do
+      describe 'true' do
+        it 'sets to Redox Available' do
+          media.availability = true
+
+          assert_equal('Available', media.availability)
+        end
+      end
+
+      describe 'false' do
+        it 'sets to Redox Available' do
+          media.availability = false
+
+          assert_equal('Unavailable', media.availability)
+        end
+      end
+
+      describe 'not boolean' do
+        it 'sets to Redox Available' do
+          media.availability = :cat
+
+          assert_equal(:cat, media.availability)
+        end
+      end
+    end
+
+    describe '#add_file' do
+      describe ' < 200KB file' do
+        let(:filepath) { 'test/samples/media.response.json' }
+
+        before do
+          media.add_filepath(filepath)
+        end
+
+
+        it 'base64 encodes' do
+          assert_equal(Base64::encode64(File.read(filepath)), media[:FileContents])
+        end
+
+        it 'adds file extension' do
+          assert_equal('JSON', media[:FileType])
+        end
+
+        it 'adds file name' do
+          assert_equal('media.response', media[:FileName])
+        end
+      end
+
+      describe ' > 200KB file' do
+        let(:filepath) { 'test/samples/bigfile.txt' }
+
+        it 'is not implemented' do
+          err = assert_raises { media.add_filepath(filepath) }
+
+          assert_equal('Not implemented', err.message)
+        end
+      end 
+    end
+  end
+end
