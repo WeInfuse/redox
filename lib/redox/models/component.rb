66e7bb47dec08a43415ac806c01449c54180f014
Enrique Urbina <btace1347@yahoo.com>
2021-11-18 13:27:31 -0600

    Adding components to notes (#34)
    * adding components to notes
    
    * adding test for note
    
    * update changeload
    
    Co-authored-by: Enrique Urbina <enrique@Devys-MacBook-Pro.local>

---
+++ lib/redox/models/component.rb
@@ -0,0 +1,13 @@
+module Redox
+  module Models
+    class Component < AbstractModel
+      property :Id, required: false, from: :id
+      property :Name, required: false, from: :name
+      property :Value, required: false, from: :value
+
+      alias_method :id, :Id
+      alias_method :name, :Name
+      alias_method :value, :Value
+    end
+  end
+end
--- lib/redox/models/note.rb
+++ lib/redox/models/note.rb
@@ -5,11 +5,13 @@ module Redox
       property :DocumentType, from: :document_type, required: false
       property :DocumentID, from: :document_id, required: false
       property :Provider, required: false, from: :provider, default: {}
+      property :Components, required: false, from: :components, default: []
 
       alias_method :content_type, :ContentType
       alias_method :document_type, :DocumentType
       alias_method :document_id, :DocumentID
       alias_method :provider, :Provider
+      alias_method :components, :Components
     end
   end
 end
