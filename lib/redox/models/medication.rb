fa708a9287c083eae2a36f7c2a997996bd5a4269
Enrique Urbina <btace1347@yahoo.com>
2022-01-25 15:58:30 -0600

    enrique/redox-med-admin-api (#39)
    * medication model update
    
    Co-authored-by: Enrique Urbina <enrique@Devys-MacBook-Pro.local>

--- lib/redox/models/medication.rb
+++ lib/redox/models/medication.rb
@@ -7,6 +7,10 @@ module Redox
       property :Rate, required: false, from: :rate, default: {}
       property :Route, required: false, from: :route, default: {}
       property :Product, required: false, from: :product, default: {}
+      property :Components, required: false, from: :components, default: []
+      property :OrderedBy, required: false, from: :ordered_by, default: {}
+      property :Indications, required: false, from: :indications, default: []
+      property :AdministeringProvider, required: false, from: :administering_provider, default: {}
 
       alias_method :order, :Order
       alias_method :lot_number, :LotNumber
@@ -14,6 +18,10 @@ module Redox
       alias_method :rate, :Rate
       alias_method :route, :Route
       alias_method :product, :Product
+      alias_method :components, :Components
+      alias_method :ordered_by, :OrderedBy
+      alias_method :indications, :Indications
+      alias_method :administering_provider, :AdministeringProvider
     end
   end
 end
